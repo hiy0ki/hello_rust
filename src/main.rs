d6f5833909e0303a05c92a77dbebd4fb4fdcf0aa
hiyoki <maha.tree.21@gmail.com>
2018-01-07 23:23:53 +0900

    for statement

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ fn main() {
     println!("Hello, world!");
 
     println!("this is {}", add_one(3));
-}
 
+    for x in 1..11 {
+        println!("{}", x);
+    }
+}
 
 fn add_one(x: i32) -> i32 {
     x + 1
